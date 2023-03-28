5ff07b5bd6388e1a432758c2bdbd58da79f9ad95
ZinovevSasha <109625848+ZinovevSasha@users.noreply.github.com>
2023-03-28 09:59:56 +0200

    add swiftLint Readme and gitIgnore

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Александр Зиновьев on 25.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .myWhite
+    }
+}
